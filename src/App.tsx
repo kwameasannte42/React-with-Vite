148ac75d9c791b7d82a99016340349b19d2d5a91
kwameasannte42 <coolllyod@gmail.com>
2023-04-11 12:40:31 -0400

    adding buttons and alerts

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
-import ListGroup from "./components/ListGroups"
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+// import ListGroup from "./components/ListGroups";
 
 function App() {
+  // let items = ["kumasi", "accra", "sunyani", "tema"];
 
-  let items = ["kumasi", "accra", "sunyani", "tema"];
+  // const handleSelectItem = (item: string) => {
+  //   console.log(item);
+  // };
 
-  const handleSelectItem = (item: string) => {
-    console.log(item)
-  }
+  const [alertVisible, setAlertVisibility] = useState(false);
 
-
-  return <div><ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/></div>
+  return (
+    <div>
+      {/* <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      /> */}
+      {/* <Alert>Hello World</Alert> */}
+      { alertVisible && <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>}
+      <Button color="info" onClick={() => setAlertVisibility(true)}>My Button</Button>
+    </div>
+  );
 }
 
-export default App
+export default App;
